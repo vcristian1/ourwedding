c2538ebeacada096998f288bb1dfd3d258a838b4
Cristian Vargas <cristian.v0223@gmail.com>
2024-04-22 17:47:25 -0500

    added small change to faw

--- src/scenes/faq/index.tsx
+++ src/scenes/faq/index.tsx
@@ -64,7 +64,7 @@ const FAQ = ({ setSelectedPage }: Props) => {
                             <svg className="flex-shrink-0 mr-2 w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                             Can I bring a plus one?
                         </h3>
-                        <p className="sm:text-[18px] sp7:text-[22px] lg:text-[18px] mbpro:text-[18px] md:text-[18px]">Unfortunately no, please do not arrive with someone who was not included in your original save the date. There will not be a seat or food available for them.</p>
+                        <p className="sm:text-[18px] sp7:text-[22px] lg:text-[18px] mbpro:text-[18px] md:text-[18px]">Unfortunately no, please do not arrive with someone who was not included in your evite. There will not be a seat or food available for them.</p>
                     </div>
                     <div className="mb-10">                        
                         <h3 className="flex items-center md:text-[18px] mbpro:text-[20px] mb-4 text-lg  sm:text-[20px] ipp:text-[22px] sp7:text-[24px] font-bold lg:text-[20px]">
